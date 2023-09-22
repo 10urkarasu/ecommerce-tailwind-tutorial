845f70c2e0cc86af2867c4edeba5b868c7c6ece0
Onur Karasu <onur.karasu@flokserkimya.com.tr>
2023-09-22 09:50:54 +0300

    added screens settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,16 @@ module.exports = {
     './src/*'
   ],
   theme: {
+    screens:{
+      sm:'576px',
+      md:'768px',
+      lg:'992px',
+      xl:'1200px'
+    },
+    container:{
+      center:true,
+      padding:'1rem'
+    },
     extend: {
       fontFamily: {
         poppins: "'Poppins', sans-serif",
